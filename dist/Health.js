0edd92651fe05300919eab1db6a0835c8a663e94
samme <samme@users.noreply.github.com>
2018-12-19 18:05:43 -0800

    Rebuild

--- dist/Health.js
+++ dist/Health.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Health = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.PhaserHealth = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var HEALTH = 'health';
 var MAX_HEALTH = 'maxHealth';
 var DAMAGE = 'damage';
@@ -65,18 +65,18 @@ var Health = {
       return this;
     },
 
-    setMaxHealth: function (amount) {
+    setMaxHealth: function (amount, silent) {
       this.setData(MAX_HEALTH, amount);
 
       if (this.getHealth() > amount) {
-        this.setData(HEALTH, amount); // silent
+        this.setHealth(amount, silent);
       }
 
       return this;
     },
 
     damage: function (amount, silent) {
-      if (amount === 0) return;
+      if (amount === 0) return this;
 
       if (!amount) amount = 1;
 
@@ -90,7 +90,7 @@ var Health = {
     },
 
     heal: function (amount, silent) {
-      if (amount === 0) return;
+      if (amount === 0) return this;
 
       if (!amount) amount = 1;
 
